1722ea8bdf9c72cb2142004c6258a52dedd544dc
Xifax <demi.log@gmail.com>
2011-12-20 13:29:34 +0400

    Module for jdic

---
+++ src/db/jdic.py
@@ -0,0 +1,31 @@
+# -*- coding=utf-8 -*-
+"""
+Describes abstractions for kanjidic2.
+Note, that jdic2 contains (only) 13K+ unique kanji.
+"""
+
+from elixir import Entity, Field, Integer, Unicode
+
+class KanjiInJDIC(Entity):
+    """
+    Kanji standard entry in kanjidic2.
+    Includes kanji itself, grade, frequency, readings and meaning.
+    """
+
+    id = Field(Integer)
+    literal = Field(Unicode)
+    grade = Field(Integer)
+    freq = Field(Integer)
+    on_reading = Field(Unicode)
+    kun_reading = Field(Unicode)
+    meaning = Field(Unicode)
+
+    def __repr__(self):
+        return u"Kanji: %s | On: %s | Kun: %s | Meaning: %s" \
+        % (self.character, self.on_reading, self.kun_reading, self.meaning)
+
+    @staticmethod
+    def search(kanji):
+        found = KanjiInJDIC.query().get(kanji)
+        if found:
+            return found
